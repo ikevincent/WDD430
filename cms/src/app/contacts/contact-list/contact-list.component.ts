d1217a004767c660bbc85e032afbb17a6ef0f45b
ikethompson88 <ikethompson88@gmail.com>
2025-01-24 20:33:43 -0800

    Troubleshooting "Display ContactListComponent" step

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contacts.model';
 
 @Component({
   selector: 'cms-contact-list',
   standalone: false,
-  
-  templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
-})
-export class ContactListComponent {
 
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.css',
+})
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact(
+      1,
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      '../../assets/images/jacksonk.jpg',
+      null
+    ),
+    new Contact(
+      2,
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      '../../assets/images/barzeer.jpg',
+      null
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
 }
+
+// import { Recipe } from '../recipe.model';
+
+// export class RecipeListComponent implements OnInit {
+//   recipes: Recipe[] = [
+//     new Recipe(
+//       'Lasagna',
+//       'Lasagna is my favorite meal!',
+//       'https://cookingwithayeh.com/wp-content/uploads/2023/12/Spinach-Lasagna-SQ-12.jpg'
+//     ),
+//   ];
