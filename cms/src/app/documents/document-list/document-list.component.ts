c7091b2f6b425d1929415e14d255a75aacef358a
ikethompson88 <ikethompson88@gmail.com>
2025-02-03 14:36:53 -0800

    All Document Components Implimented

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   standalone: false,
-  
-  templateUrl: './document-list.component.html',
-  styleUrl: './document-list.component.css'
-})
-export class DocumentListComponent {
 
+  templateUrl: './document-list.component.html',
+  styleUrl: './document-list.component.css',
+})
+export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents: Document[] = [
+    new Document(
+      1,
+      'W01 Assignment',
+      'Completed W01 assignment',
+      'http://example.com/doc1'
+    ),
+    new Document(
+      2,
+      'W02 Assignment',
+      'Completed W02 assignment',
+      'http://example.com/doc2'
+    ),
+    new Document(
+      3,
+      'W03 Assignment',
+      'Completed W03 assignment',
+      'http://example.com/doc3'
+    ),
+    new Document(
+      4,
+      'W04 Assignment',
+      'Completed W04 assignment',
+      'http://example.com/doc4'
+    ),
+    new Document(
+      5,
+      'W05 Assignment',
+      'Completed W05 assignment',
+      'http://example.com/doc5'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onSelectDocument(document: Document): void {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
